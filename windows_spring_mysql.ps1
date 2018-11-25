5a7b8d4b91fb79f1710b3aee73c63111150b23cc
pointcircle <45114306+pointcircle@users.noreply.github.com>
2018-11-25 16:52:55 +0800

    Update windows_spring_mysql.ps1

--- windows_spring_mysql.ps1
+++ windows_spring_mysql.ps1
@@ -1,4 +1,5 @@
 # https://www.microsoft.com/en-us/download/details.aspx?id=54616
+# https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip
 
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 
